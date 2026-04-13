cf3d2ff2594565916c429c9a7e4415f45d2575cc
5hun-s <>
2026-04-14 01:04:08 +0900

    rspecとjest追加

---
+++ frontend/jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/*.test.[jt]s?(x)"],
+};
+
+export default createJestConfig(config);
